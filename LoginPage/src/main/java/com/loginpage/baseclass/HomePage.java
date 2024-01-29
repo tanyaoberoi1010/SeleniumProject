2a7c3727f172217100318b87bb8c81af5733f186
tanyaoberoi <tanyaoberoi@localhost>
2024-01-29 03:34:12 -0800

    Update

---
+++ LoginPage/src/main/java/com/loginpage/baseclass/HomePage.java
@@ -0,0 +1,28 @@
+package com.loginpage.baseclass;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    WebDriver driver;
+
+    By homePageUserName = By.xpath("//table//tr[@class='heading3']");
+
+    
+
+    public HomePage(WebDriver driver){
+
+        this.driver = driver;
+
+    }
+
+    //Get the User name from Home Page
+
+        public String getHomePageDashboardUserName(){
+
+         return    driver.findElement(homePageUserName).getText();
+
+        }
+
+}
--- LoginPage/src/main/java/com/loginpage/pageobjects/Login.java
+++ LoginPage/src/main/java/com/loginpage/pageobjects/Login.java
@@ -1,44 +1,85 @@
 package com.loginpage.pageobjects;
 
-import org.checkerframework.checker.units.qual.t;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Login {
 
-	public static void main(String[] args) {
-		
-	
-	WebDriver driver=new ChromeDriver();
-	driver.get("https://www.facebook.com/");
-	driver.manage().window().maximize();
-	
-	//System.out.println("Ok");
-	WebElement username=driver.findElement(By.id("email"));
-    WebElement password=driver.findElement(By.name("pass"));
-    WebElement loginbtn=driver.findElement(By.name("login"));
-    //System.out.println("Ok");
-	username.sendKeys("tanyaoberoi1010@gmail.com");
-	password.sendKeys("samjhawan");
-	//System.out.println("Ok");
-	loginbtn.click();
-	
-	
-	String current_url=driver.getCurrentUrl();
-	
-	if(current_url.toString()== "https://www.facebook.com/home.php")
-	{
-		System.out.println("Login is succesful");
-	
-	}
-	
-	else
-	{
-		System.out.println("Invalid Login");
-	}
-	
-}
-}
+    WebDriver driver;
 
+    By user99GuruName = By.name("uid");
+
+    By password99Guru = By.name("password");
+
+    By titleText =By.className("barone");
+
+    By login = By.name("btnLogin");
+
+    public Login(WebDriver driver){
+
+        this.driver = driver;
+
+    }
+
+    //Set user name in textbox
+
+    public void setUserName(String strUserName){
+
+        driver.findElement(user99GuruName).sendKeys(strUserName);
+
+    }
+
+    //Set password in password textbox
+
+    public void setPassword(String strPassword){
+
+         driver.findElement(password99Guru).sendKeys(strPassword);
+
+    }
+
+    //Click on login button
+
+    public void clickLogin(){
+
+            driver.findElement(login).click();
+
+    }
+
+    //Get the title of Login Page
+
+    public String getLoginTitle(){
+
+     return    driver.findElement(titleText).getText();
+
+    }
+
+    /**
+
+     * This POM method will be exposed in test case to login in the application
+
+     * @param strUserName
+
+     * @param strPasword
+
+     * @return
+
+     */
+
+    public void loginToGuru99(String strUserName,String strPasword){
+
+        //Fill user name
+
+        this.setUserName(strUserName);
+
+        //Fill password
+
+        this.setPassword(strPasword);
+
+        //Click Login button
+
+        this.clickLogin();        
+    }
+
+}
