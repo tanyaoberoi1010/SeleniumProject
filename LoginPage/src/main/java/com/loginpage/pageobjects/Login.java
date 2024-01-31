ebdc74a1c4fa9d4a4695b4b0ed7a89cfad2db07c
tanyaoberoi <tanyaoberoi@localhost>
2024-01-30 18:58:49 -0800

    Final Valid Login

--- LoginPage/src/main/java/com/loginpage/pageobjects/Login.java
+++ LoginPage/src/main/java/com/loginpage/pageobjects/Login.java
@@ -17,9 +17,9 @@ static ConfigFileReader configFileReader = new ConfigFileReader();
           this.driver = driver;
 	}
 	
-	By uName = By.name("user[login]");
+	private By uName = By.name("user[login]");
 	
-	By pswd = By.name("user[password]");
+	private By pswd = By.name("user[password]");
 	
 	By loginBtn=By.xpath("//button[@type='submit']");
 	
